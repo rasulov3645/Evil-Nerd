2d690ea387bd23a4a49c875c98c1054677c25b62
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 12:50:42 +0300

    Урок 16 - Конструкторы.

---
+++ lesson016/lesson16/src/Bender.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Bender {
+    public static void main(String[] args) {
+        Fry PhillipFry = new Fry("Interstellar");
+        Fry PFry = new Fry("Teminator");
+
+        PhillipFry.output();
+        PFry.output();
+
+    }
+
+}
