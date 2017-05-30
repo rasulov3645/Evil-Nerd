54b841bc44b590940c052eb711e8bc942cd64d56
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 10:56:33 +0300

    Урок 12 - Использование нескольких классов.

---
+++ lesson012/lesson12/src/com/gamzat/Bender.java
@@ -0,0 +1,16 @@
+package com.gamzat;
+
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Bender {
+    int age;
+    public static void main(String[] args) {
+        Fry PhillipFry = new Fry();
+        PhillipFry.FrySay();
+    }
+
+    public void methd () {
+        System.out.println("Я класс Bendera");
+    }
+}
---
+++ lesson012/lesson12/src/com/gamzat/Fry.java
@@ -0,0 +1,15 @@
+package com.gamzat;
+
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Fry {
+    public void FrySay () {
+        System.out.println("Это класс Фрая!");
+    }
+
+    public static void main(String[] args) {
+        Bender bender = new Bender();
+        bender.methd();
+    }
+}
