4765ccec3878eb89a042e80fff603526c79eda84
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 13:06:00 +0300

    Урок 17 - Вложенные условия.

---
+++ lesson017/lesson17/src/Bender.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Bender {
+    public static void main(String[] args) {
+        int Beer  = 5;   // Пиво
+
+        if (Beer < 4){
+            System.out.println("Мне надо еще пива!!!");
+        }
+        else {
+            if (Beer > 6) {
+                System.out.println("Кажется я перебрал!");
+            }
+            else {
+                System.out.println("Мне уже достаточно!");
+            }
+        }
+    }
+}
