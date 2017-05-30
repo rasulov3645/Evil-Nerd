da066977fbb72f4fb75179d5254f9f1cb9f54f4d
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 05:18:59 +0300

    Урок 3 - Иерархия Java и первая программа

---
+++ lesson003/Index/src/com/gamzat/Bender.java
@@ -0,0 +1,21 @@
+package com.gamzat;
+
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Bender {
+    // переменные
+        // команды
+            // методы
+    int a;
+    String b = "Hi";
+
+    public static String cool() {
+        return "Я крут!";
+    }
+
+    public static void main(String[] args){
+        System.out.println("Привет, мешок с костями.");
+    }
+
+}
---
+++ lesson003/Index/src/com/gamzat/Main.java
@@ -0,0 +1,20 @@
+// Урок 3 - Иерархия Java и первая программа
+// sout + Enter = System.out.println();
+// Быстро скипировать строку с помущью 'intellij ide'
+// наводим мышью на строку и нажимаем на клавиатуре 'ctrl+d'
+
+// Метод main можно быстро напечатот
+// psvm + Enter = public static void main(String[] args) { }
+// Крут даже
+
+package com.gamzat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+        System.out.println("Hi");
+        System.out.println("привет");
+        System.out.println("здрасьте");
+    }
+}
