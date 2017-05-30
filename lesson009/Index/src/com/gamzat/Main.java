b5a6c55524907af2df74da4edd55d8b46e825234
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 07:15:58 +0300

    Урок 9 - Логические операторы и оператор if ().

---
+++ lesson009/Index/src/com/gamzat/Main.java
@@ -0,0 +1,34 @@
+package com.gamzat;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int Homer = 110;
+        int Peter = 120;
+
+        // &&(и), ||(или)
+        if (Homer > 100 || Peter > 100 ) {
+            System.out.println("Идеальный вес!");
+        }
+        else {
+            System.out.println("Иди еще поешь, дистрофик!");
+        }
+
+        /*
+        int Fry = 20;
+
+        if (Fry  == 15) {
+            System.out.println("Тест пройден. Фраю " + Fry + "лет.");
+        }
+        else {
+            System.out.println("Тест не пройден. Фраю не " + Fry + "лет.");
+        }
+        */
+
+
+
+
+
+    }
+}
