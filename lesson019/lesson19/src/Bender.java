7831cad7ff1c67ffee9fd2afdc54e897d406846e
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 13:57:35 +0300

    Урок 19 - Условные операторы.

---
+++ lesson019/lesson19/src/Bender.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Гамзат on 30.05.2017.
+ *
+ * Урок 19 - Условные операторы
+ *
+ */
+public class Bender {
+    public static void main(String[] args) {
+        int Homer;
+        Homer = 110;
+
+        System.out.println(Homer > 115 ? "Да, Гомер очень жирный!!!" : "Гомер довольно спортивный чувак!!!");
+        
+    }
+}
---
+++ lesson019/lesson19/src/Fry.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Fry {
+
+}
