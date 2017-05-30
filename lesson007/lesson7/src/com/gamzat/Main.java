c1c3fb6e8f5cc081de28f1c3c886ffd3bba7ed06
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 06:26:15 +0300

    Урок 7 - Создание калькулятора на Java.

---
+++ lesson007/lesson7/src/com/gamzat/Main.java
@@ -0,0 +1,23 @@
+package com.gamzat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Bender = new Scanner(System.in);
+        double FirstNumber;
+        double SecondNumber;
+        double Result;
+        //double FirstNumber, SecondNumber, Result
+
+
+        System.out.println("Введите первое число: ");
+        FirstNumber = Bender.nextDouble();
+        System.out.println("Введите второе число: ");
+        SecondNumber = Bender.nextDouble(); 
+
+        Result = FirstNumber + SecondNumber;
+        System.out.println("Сумма равна: " + Result);
+
+    }
+}
