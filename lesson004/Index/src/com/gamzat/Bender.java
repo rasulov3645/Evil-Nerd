abe93ab1fda09cc61c16d60fbf314febc8dbff54
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-30 05:44:17 +0300

    Урок 4 - Переменные и вывод на экран

---
+++ lesson004/Index/src/com/gamzat/Bender.java
@@ -0,0 +1,19 @@
+package com.gamzat;
+
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Bender {
+
+    public static void main (String [] args ){
+        double Bart;
+        Bart = 460.128;
+
+        System.out.println("Я хочу ");
+        System.out.println(Bart);
+        System.out.println("долларов в день!");
+        
+    }
+
+
+}
---
+++ lesson004/Index/src/com/gamzat/Index.java
@@ -0,0 +1,10 @@
+package com.gamzat;
+
+/**
+ * Created by Гамзат on 30.05.2017.
+ */
+public class Index {
+
+
+
+}
