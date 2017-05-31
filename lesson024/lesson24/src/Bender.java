195ef08b926ceebb86fd5e27b6adccb0cc90bcb4
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-05-31 23:47:31 +0300

    Урок 24 - Класс Math.

---
+++ lesson024/lesson24/src/Bender.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Гамзат on 31.05.2017.
+ */
+public class Bender {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-51));
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.floor(2.9999));
+        System.out.println(Math.max(5.6, 4.2));
+        System.out.println(Math.min(5.6, 4.2));
+        System.out.println(Math.pow(4, 2));
+        System.out.println(Math.sqrt(16));
+        
+
+    }
+}
---
+++ lesson024/lesson24/src/Fry.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Гамзат on 31.05.2017.
+ */
+public class Fry {
+}
