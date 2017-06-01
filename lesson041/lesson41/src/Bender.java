66ea07f2b00a5c3fbde9e1afcfa94e9699a6a097
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-06-01 19:37:05 +0300

    Урок 41 - Метод toString

---
+++ lesson041/lesson41/src/Bender.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Гамзат on 01.06.2017.
+ *
+ * Урок 41 - Метод toString
+ */
+public class Bender {
+    public static void main(String[] args) {
+        Rick bd = new Rick(16, 6, 1906);
+    }
+}
---
+++ lesson041/lesson41/src/Rick.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Гамзат on 01.06.2017.
+ */
+public class Rick {
+    private int day;
+    private int month;
+    private int year;
+
+    public Rick(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+        System.out.printf("День рождения Рика - %s\n", this);
+    }
+
+    public String toString() {
+        return String.format("%d/%d/%d", day, month, year);
+    }
+}
+
+
