fd349b5c69c2489d00ce311d9ddc5a1409ead4dc
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-06-01 15:14:30 +0300

    Урок 39 - Перегруженные конструкторы, set и get методы (часть 2)

---
+++ lesson039/lesson39/src/Bender.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Гамзат on 01.06.2017.
+ *
+ * Урок 39 - Перегруженные конструкторы, set и get методы (часть 2)
+ */
+public class Bender {
+    public static void main(String[] args) {
+        Fry PhillipFry1 = new Fry();
+        Fry PhillipFry2 = new Fry(2000);
+        Fry PhillipFry3 = new Fry(2005, 20);
+        Fry PhillipFry4 = new Fry(2012, 12, 12);
+
+        System.out.printf("%s\n", PhillipFry1.toDisplay());
+        System.out.printf("%s\n", PhillipFry2.toDisplay());
+        System.out.printf("%s\n", PhillipFry3.toDisplay());
+        System.out.printf("%s\n", PhillipFry4.toDisplay());
+        
+    }
+
+}
